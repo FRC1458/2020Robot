0e890aacf0b85614b7bca554a67cb8824d79e93c
Tabernacle8 <unconfigured@null.spigotmc.org>
2020-02-17 12:43:03 -0800

    turtwig

--- src/main/kotlin/frc/team1458/robot/ColorSensorConnect.kt
+++ src/main/kotlin/frc/team1458/robot/ColorSensorConnect.kt
@@ -1,14 +1,9 @@
 package frc.team1458.robot
 
 //Recommended imports
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.util.Color;
-
-import com.revrobotics.ColorSensorV3;
-import com.revrobotics.ColorMatchResult;
-import com.revrobotics.ColorMatch;
+import edu.wpi.first.wpilibj.I2C
+import com.revrobotics.ColorSensorV3
+import com.revrobotics.ColorMatch
 
 
 class ColorSensorConnect {
@@ -22,7 +17,7 @@ class ColorSensorConnect {
     val kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114)
     val kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113)
 
-    init{
+    init {
         m_colorMatcher.addColorMatch(kBlueTarget)
         m_colorMatcher.addColorMatch(kGreenTarget)
         m_colorMatcher.addColorMatch(kRedTarget)
@@ -30,13 +25,13 @@ class ColorSensorConnect {
     }
 
 
-    fun update() : String{
+    fun update(): String {
         val match = m_colorMatcher.matchClosestColor(m_colorSensor.getColor())
 
         if (match.color === kBlueTarget) return("Blue")
         if (match.color === kRedTarget) return("Red")
         if (match.color === kGreenTarget) return("Green")
         if (match.color === kYellowTarget) return("Yellow")
-        return("WHY ANISH SO DUMMY THICCC THO")
+        return("turtwig")
     }
 }
